52e8a98383ffe971b0ad3e2af90365ba696df2b2
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-09-18 11:26:04 +0200

    feat(site): add site.ts

---
+++ app/lib/site.ts
@@ -0,0 +1,3 @@
+export const siteUrl = "https://www.jakub-szczucinski.com";
+export const siteTitle = "Jakub Szczucinski | Software Developer Portfolio";
+export const siteDescription = "Explore Jakub Szczucinski's software, web and game development projects, technical skills, education and professional experience.";
